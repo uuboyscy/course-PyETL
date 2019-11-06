85fb3abdbd7822ec95607f6a0e5debf1c636ec27
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2019-11-06 21:40:51 +0800

    Update 02_actualExample.py

---
+++ part4_requestWithPost/02_actualExample.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://buzzorange.com/techorange/'
+url_post = 'https://buzzorange.com/techorange/wp-admin/admin-ajax.php'
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+
+for page in range(0,10):
+    post_data = {
+        'action': 'fm_ajax_load_more',
+        'nonce': '2690da603f',
+        'page': page +1
+                 }
+
+    res = requests.post(url_post, headers=headers, data=post_data)
+    # print(res.text)
+
+    data_dict = json.loads(res.text)
+    html = data_dict['data']
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for t in soup.select('h4 a'):
+        print(t.text)
+        print(t['href'])
+        print()

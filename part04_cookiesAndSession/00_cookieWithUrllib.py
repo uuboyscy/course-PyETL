8282b82870b03809de1fb9fc3e0822bbd0136d0b
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2020-09-29 22:10:32 +0800

    Update 00_cookieWithUrllib.py

---
+++ part04_cookiesAndSession/00_cookieWithUrllib.py
@@ -0,0 +1,22 @@
+# 若為Mac電腦，請先貼上此段程式碼
+########### For Mac user ###########
+import os
+import ssl
+# used to fix Python SSL CERTIFICATE_VERIFY_FAILED
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+####################################
+
+from urllib import request
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Cookie': 'over18=1'
+}
+
+url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+
+req = request.Request(url=url, headers=headers)
+res = request.urlopen(req)
+
+print(res.read().decode('utf-8'))

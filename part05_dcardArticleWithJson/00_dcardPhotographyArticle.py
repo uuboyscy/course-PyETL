6b80e0d1397703297d1c7dce663ecf0ccd95a194
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2020-10-04 20:00:12 +0800

    Update 00_dcardPhotographyArticle.py

--- part05_dcardArticleWithJson/00_dcardPhotographyArticle.py
+++ part05_dcardArticleWithJson/00_dcardPhotographyArticle.py
@@ -1,24 +1,14 @@
-# 若為Mac電腦，請先貼上此段程式碼
-########### For Mac user ###########
-import os
-import ssl
-# used to fix Python SSL CERTIFICATE_VERIFY_FAILED
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-####################################
-
 import requests
-from bs4 import BeautifulSoup
 import json
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 
-for i in range(0, 3):
-    url = 'https://www.dcard.tw/_api/forums/photography/posts?popular=false&limit=30&before=232004767'
+# url = 'https://www.dcard.tw/_api/forums/photography/posts?popular=false&limit=30&before=232004767'
+url = 'https://www.dcard.tw/service/api/v2/forums/photography/posts?limit=24&before=234542096'
 
+for i in range(0, 5):
     res = requests.get(url, headers = headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    json_string = str(soup)
+    json_string = str(res.text)
     js = json.loads(json_string)
 
     last_id = js[len(js)-1]['id']
@@ -28,4 +18,5 @@ for i in range(0, 3):
         print('https://www.dcard.tw/f/photography/p/' + str(each_article['id']))
         print()
 
-    url = 'https://www.dcard.tw/_api/forums/photography/posts?popular=false&limit=30&before=%s'%(last_id)
+    # url = 'https://www.dcard.tw/_api/forums/photography/posts?popular=false&limit=30&before=%s'%(last_id)
+    url = 'https://www.dcard.tw/service/api/v2/forums/photography/posts?limit=24&before=%s'%(last_id)

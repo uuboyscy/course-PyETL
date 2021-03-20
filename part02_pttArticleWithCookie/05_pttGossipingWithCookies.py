9070b3137d8ae1729e0a29769ea3e0e830c1f9e3
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2021-03-20 09:00:22 +0800

    Update 05_pttGossipingWithCookies.py

--- part02_pttArticleWithCookie/05_pttGossipingWithCookies.py
+++ part02_pttArticleWithCookie/05_pttGossipingWithCookies.py
@@ -1,10 +1,3 @@
-# 若為Mac電腦，請先貼上此段程式碼
-########### For Mac user ###########
-import ssl
-# used to fix Python SSL CERTIFICATE_VERIFY_FAILED
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-####################################
 import time
 import os
 import requests

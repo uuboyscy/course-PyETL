1942e88e5c00c4ef56cde90ac16e53f2a23e07cc
uuboyscy <aegis12321@gmail.com>
2022-06-08 22:14:17 +0800

    Updated new codes for Selenium-part

--- part07_selenium/00_seleniumUsage.py
+++ part07_selenium/00_seleniumUsage.py
@@ -1,24 +1,29 @@
-# from selenium.webdriver import Chrome
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
-options = webdriver.ChromeOptions()
+###### Deprecated ######
+# options = ChromeOptions()
 # options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 # print(options.binary_location)
-# driver = webdriver.Chrome('./chromedriver', options=options)
-driver = webdriver.Chrome('./chromedriver')
+# driver = Chrome('./chromedriver', options=options)
+# driver = Chrome('./chromedriver')
+########################
 
 service = Service("./chromedriver")
-driver = webdriver.Chrome(service=service)
+driver = Chrome(service=service)
 
 url = 'https://www.ptt.cc/bbs/index.html'
 
 driver.get(url)
-driver.find_element_by_class_name('board-name').click()
-driver.find_element_by_class_name('btn-big').click()
+# driver.find_element_by_class_name('board-name').click() # Deprecated
+driver.find_element(by=By.CLASS_NAME, value='board-name').click()
+# driver.find_element_by_class_name('btn-big').click() # Deprecated
+driver.find_element(by=By.CLASS_NAME, value='btn-big').click()
 
 cookie = driver.get_cookies()
 

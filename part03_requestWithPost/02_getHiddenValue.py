e9a52d4227193a6ca3ee0d4a1a1590936dea2374
uuboyscy <aegis12321@gmail.com>
2022-06-20 22:12:49 +0800

    Updated

--- part03_requestWithPost/02_getHiddenValue.py
+++ part03_requestWithPost/02_getHiddenValue.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 
-url = 'https://web.pcc.gov.tw/tps/pss/tender.do?method=goSearch&searchMode=common&searchType=advance&searchTarget=ATM'
+url = 'https://testselect.uuboyscy.repl.co/'
 
 res = requests.get(url, headers=headers)
 soup = BeautifulSoup(res.text, 'html.parser')

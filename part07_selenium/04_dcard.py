33c0c8880bdb8b62c1912f5d6be838a5ef20fb1c
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2022-10-16 16:11:10 +0800

    Update 04_dcard.py

--- part07_selenium/04_dcard.py
+++ part07_selenium/04_dcard.py
@@ -1,4 +1,5 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 import time
 
 driver = Chrome('./chromedriver')
@@ -8,11 +9,12 @@ driver.get(url)
 time.sleep(5)
 
 # 在表單輸入特定關鍵字
-driver.find_element_by_tag_name('input').send_keys('攝影')
+# driver.find_element_by_tag_name('input').send_keys('攝影')
+driver.find_element(by=By.TAG_NAME, value='input').send_keys('攝影')
 time.sleep(5)
 
 # 按下查詢按鈕
-driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div[1]/div/div/form/button[2]').click()
+driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div[1]/div/div[1]/div/div/form/button[2]').click()
 time.sleep(5)
 
 # 將網頁畫面往下滾動至離頂部 5000 高度的位子
@@ -29,4 +31,3 @@ time.sleep(5)
 # 取得目前的 html 字串
 html = driver.execute_script("return document.getElementsByTagName('html')[0].outerHTML")
 print(html)
-

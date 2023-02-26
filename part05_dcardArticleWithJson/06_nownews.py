d67590d67a89723a5385c002e9e69d895d98a5bd
uuboyscy <aegis12321@gmail.com>
2023-02-26 15:39:01 +0800

    Update: Nownews

---
+++ part05_dcardArticleWithJson/06_nownews.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# https://www.nownews.com/cat/column/
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+
+url = "https://www.nownews.com/nn-client/api/v1/cat/column/?pid=6065571"
+
+res = requests.get(url, headers=headers)
+
+print(res.json())

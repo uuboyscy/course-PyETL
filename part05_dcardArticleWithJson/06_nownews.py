b5b72cd8f4c4d9bdeba7bce6b906f00337879def
uuboyscy <aegis12321@gmail.com>
2023-11-05 14:29:36 +0800

    Nownews flow

--- part05_dcardArticleWithJson/06_nownews.py
+++ part05_dcardArticleWithJson/06_nownews.py
@@ -3,11 +3,23 @@ import requests
 # https://www.nownews.com/cat/column/
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
 }
 
 url = "https://www.nownews.com/nn-client/api/v1/cat/column/?pid=6294091"
 
 res = requests.get(url, headers=headers, timeout=600)
 
-print(res.json())
+# print(res.json())
+
+for data in res.json()["data"]["newsList"]:
+    article_id = data["id"]
+    article_title = data["postTitle"]
+    article_url = "https://www.nownews.com" + data["postUrl"]
+    article_image_url = data["imageUrl"]
+
+    print(article_id)
+    print(article_title)
+    print(article_url)
+    print(article_image_url)
+    print("========")

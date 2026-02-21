32ff646c41d87605213d0371ee54ad9e7d763949
uuboyscy <aegis12321@gmail.com>
2026-02-21 13:20:58 +0800

    fix(part07_selenium): :zap: New headless argument

--- part07_selenium/04_dcard.py
+++ part07_selenium/04_dcard.py
@@ -8,7 +8,7 @@ chrome_options = Options()
 
 
 # 基本設定
-# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
